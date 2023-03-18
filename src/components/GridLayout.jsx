d0078078cdd6e6da6a5c25924d5cc4652d64745c
renkert2 <renkert2@illinous.edu>
2023-03-18 16:49:26 -0500

    Experiments with horizontal scroll didnt go well

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,18 +1,52 @@
 import React from "react"
+import ReactDOM from 'react-dom';
 import styled from "@emotion/styled"
 import dimensions from "styles/dimensions"
 
-const GridLayout = styled("div")`
+const StyledGrid = styled("div")`
   display: grid;
-  grid-template-columns: repeat(auto-fit, 28.3vw);
-  grid-auto-flow: column;
-  grid-template-rows: repeat(2, 47.5vw);
-  column-gap: 5vw;
-  row-gap: 5vw;
+  grid-column-gap: ${dimensions.homeAlignLeft};
+  grid-row-gap: ${dimensions.homeAlignLeft};
+  /* scroll-snap-type: x mandatory; */
+  padding-top: ${dimensions.homeVerticalMargin};
+  padding-bottom: ${dimensions.homeVerticalMargin};
+  padding-left: ${dimensions.homeAlignLeft};
+  padding-right: ${dimensions.homeAlignLeft};
+  
+  @media (max-aspect-ratio: 10/16) {
+    grid-auto-flow: row;
+    grid-template-columns: 100%;
+    grid-auto-rows: calc(calc(100%/3) - calc(${dimensions.homeAlignLeft}*2/3));
+    overflow-y: scroll;
+  }
+
+  @media (max-aspect-ratio: 3/4) {
+    grid-auto-flow: row;
+    grid-template-columns: 100%;
+    grid-auto-rows: calc(calc(100%/2) - calc(${dimensions.homeAlignLeft}/2));
+    overflow-y: scroll;
+  }
+
+  @media (min-aspect-ratio: 3/4) {
+    grid-auto-flow: column;
+    overflow-x: scroll;
+    grid-auto-columns: calc(calc(100%/2) - calc(${dimensions.homeAlignLeft}/2));
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  @media (min-aspect-ratio: 4/3) {
+    grid-auto-columns: calc(calc(100%/3) - calc(${dimensions.homeAlignLeft}*2/3));
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  @media (min-aspect-ratio: 16/10) {
+    grid-auto-columns: calc(calc(100%/4) - calc(${dimensions.homeAlignLeft}*3/4));
+    grid-template-rows: repeat(2, 1fr);
+  }
 `
 
-export default class Grid extends React.Component {
+export default class GridLayout extends React.Component {
   render() {
-    return <GridLayout>{this.props.children}</GridLayout>
+    return <StyledGrid>{this.props.children}</StyledGrid>
   }
 }
--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "@emotion/styled"
 import dimensions from "styles/dimensions"
 import ProjectCard from "components/ProjectCard"
+import GridLayout from "components/GridLayout"
 
 const HorizontalContainer = styled("div")`
   width: 100vw;
@@ -10,76 +11,11 @@ const HorizontalContainer = styled("div")`
   flex-grow: 1;
 `
 
-const GridLayout = styled("div")`
-  display: grid;
-  grid-column-gap: ${dimensions.homeAlignLeft};
-  grid-row-gap: ${dimensions.homeAlignLeft};
-  /* scroll-snap-type: x mandatory; */
-  padding-top: ${dimensions.homeVerticalMargin};
-  padding-bottom: ${dimensions.homeVerticalMargin};
-  padding-left: ${dimensions.homeAlignLeft};
-  padding-right: ${dimensions.homeAlignLeft};
-  
-  @media (max-aspect-ratio: 10/16) {
-    grid-auto-flow: row;
-    grid-template-columns: 100%;
-    grid-auto-rows: calc(calc(100%/3) - calc(${dimensions.homeAlignLeft}*2/3));
-    overflow-y: scroll;
-  }
-
-  @media (max-aspect-ratio: 3/4) {
-    grid-auto-flow: row;
-    grid-template-columns: 100%;
-    grid-auto-rows: calc(calc(100%/2) - calc(${dimensions.homeAlignLeft}/2));
-    overflow-y: scroll;
-  }
-
-  @media (min-aspect-ratio: 3/4) {
-    grid-auto-flow: column;
-    overflow-x: scroll;
-    grid-auto-columns: calc(calc(100%/2) - calc(${dimensions.homeAlignLeft}/2));
-    grid-template-rows: repeat(2, 1fr);
-  }
-
-  @media (min-aspect-ratio: 4/3) {
-    grid-auto-columns: calc(calc(100%/3) - calc(${dimensions.homeAlignLeft}*2/3));
-    grid-template-rows: repeat(2, 1fr);
-  }
-
-  @media (min-aspect-ratio: 16/10) {
-    grid-auto-columns: calc(calc(100%/4) - calc(${dimensions.homeAlignLeft}*3/4));
-    grid-template-rows: repeat(2, 1fr);
-  }
-`
-
-const handleOnWheel = (e) => {
-  const strength = Math.abs(e.deltaY);
-  if (e.deltaY === 0) return;
-
-  const el = e.currentTarget;
-  if (
-    !(el.scrollLeft === 0 && e.deltaY < 0) &&
-    !(
-      el.scrollWidth -
-        el.clientWidth -
-        Math.round(el.scrollLeft) ===
-        0 && e.deltaY > 0
-    )
-  ) {
-    e.preventDefault();
-  }
-  el.scrollTo({
-    left: el.scrollLeft + e.deltaY,
-    // large scrolls with smooth animation behavior will lag, so switch to auto
-    behavior: strength > 70 ? "auto" : "smooth",
-  });
-}
-
 export default class ProjectGrid extends React.Component {
   render() {
     return (
       <HorizontalContainer>
-          <GridLayout onWheel={handleOnWheel}>
+          <GridLayout>
             {this.props.projects.map((project, i) => (
               <ProjectCard
                 key={i}
